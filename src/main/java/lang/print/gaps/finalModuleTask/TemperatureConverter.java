85156e621d1d197030f32df5e6a4b0934afeb15e
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-16 17:13:56 -0600

    Operations implemented

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float result = (temperatureCelsius*(9/5f))+32;
+        System.out.println(result);
+
     }
 }
